810bb1e38364eae8acde302b52f29bfca2ce81fb
Sammy <samee1@navr.com>
2022-11-08 10:09:53 +0900

    First commit Learning vue js

---
+++ vue/project/src/router/index.js
@@ -0,0 +1,69 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    // 사이즈가 크거나 사용자가 바로 접근할 확률이 높은 경우
+    // component: () => import(/* webpackChunkName: "about", webpackPrefetch:true */ '../views/AboutView.vue')
+    // 빈도수가 낮거나 컨텐츠가 적어서 서버에서 바로 띄워도 문제가 없는 경우
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/dataBinding/string',
+    name: 'DataBindingStringView',
+    component: () =>
+      import(
+        /* webpackChunkName: "databinding", webpackPrefetch:true */ '../views/1_databinding/DataBindingStringView.vue'
+      )
+  },
+  {
+    path: '/dataBinding/html',
+    name: 'DataBindingHtmlView',
+    component: () =>
+      import(
+        /* webpackChunkName: "databinding", webpackPrefetch:true */ '../views/1_databinding/DataBindingHtmlView.vue'
+      )
+  },
+  {
+    path: '/dataBinding/input',
+    name: 'DataBindingInputView',
+    component: () =>
+      import(
+        /* webpackChunkName: "databinding", webpackPrefetch:true */ '../views/1_databinding/DataBindingInputView.vue'
+      )
+  },
+  {
+    path: '/dataBinding/select',
+    name: 'DataBindingSelectView',
+    component: () =>
+      import(
+        /* webpackChunkName: "databinding", webpackPrefetch:true */ '../views/1_databinding/DataBindingSelectView.vue'
+      )
+  },
+  {
+    path: '/dataBinding/check',
+    name: 'DataBindingCheckboxView',
+    component: () =>
+      import(
+        /* webpackChunkName: "databinding", webpackPrefetch:true */ '../views/1_databinding/DataBindingCheckboxView.vue'
+      )
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ vue/project/src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Hello {{ userName }}</h1>
+        <p>{{ message }}</p>
+    </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      userName: 'Sammy Hong',
+      message: 'Welcome!',
+      arr: [],
+      obj: {}
+    }
+  }
+}
+</script>
